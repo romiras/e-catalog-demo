28ff5630e3e66b5fe1cc1e1aa1396847910fe3f1
Romiras <romeeras@gmail.com>
2016-02-27 16:50:01 +0200

    Added ActiveAdmin, model Poster; basic displaying list of posters on home page

---
+++ app/admin/poster.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register Poster do
+  
+  permit_params :name, :sku, :price
+
+end
---
+++ app/models/poster.rb
@@ -0,0 +1,11 @@
+class Poster < ActiveRecord::Base
+
+  validates :name, :sku, :price,
+    presence: true
+
+  validates :name, length: { in: 2..100 }
+
+  validates :price,
+    numericality: { greater_than_or_equal_to: 1.0 }
+
+end
